00630c6821525de1865d84a5897d05407bea83c8
Tanner Koza <jtk0018@auburn.edu>
2022-03-25 17:28:46 -0500

    p1 chk 1

---
+++ problems/p1.m
@@ -0,0 +1,116 @@
+%% Optimal Estimation - Homework 3 - Problem 1
+
+clear
+clc
+close all
+
+%% Part A
+
+% Simulation Initialization
+t_end = 100;
+dt = 0.1;
+t = dt:dt:t_end;
+numSamps = length(t);
+
+% Model Initialization
+A_CL = [0 1; -1 -1.4];
+A_D = expm(A_CL*dt);
+C = [1 0];
+B_W = [0; 1];
+
+% Preallocation
+x = zeros(2, numSamps);
+y = zeros(numSamps,1);
+
+for i = 1:numSamps-1
+
+    % Discrete Simulation
+%     x(:,i+1) = A_D * x(:,i) + B_W * (2*randn);
+
+    % Continuous Simulation
+    x_dot = A_CL * x(:,i) + B_W * (2*randn);
+    x(:,i+1) = x(:,i) + x_dot * dt;
+
+    % Measurement Simulation
+    y(i+1) = x(1,i+1) + 1*randn;
+end
+
+% Simulation Plotting
+figure
+plot(t,x(1,:),'.')
+hold on
+plot(t,y,'.')
+title('Position Simulation')
+legend('Actual', 'Measurement')
+xlabel('Time (s)')
+ylabel('Value')
+
+%% Part B
+
+% Q = E[w w']
+
+Q = 4;
+Q_D = B_W*Q*B_W';
+R_D = 75;
+
+%% Part C
+
+% Preallocation & Initialization
+x_hat = zeros(2, numSamps);
+P = [0 0; 0 0];
+priorP = zeros(numSamps,2);
+postP = zeros(numSamps,2);
+
+for i = 1:numSamps-1
+    
+    % Time Update (Propagation - a priori)
+    x_hat(:,i+1) = A_D * x_hat(:,i);
+
+    P = A_D * P * A_D' + Q_D;
+    priorP(i+1,1) = P(1,1);
+    priorP(i+1,2) = P(2,2);
+
+    % Measurement Update (Correction - a posteriori)
+    K = P * C' * (C*P*C' + R_D)^-1;
+
+    x_hat(:,i+1) = x_hat(:,i+1) + K * (y(i+1) - C * x_hat(:,i+1));
+
+    P = (eye(2) - K*C) * P;
+    postP(i+1,1) = P(1,1);
+    postP(i+1,2) = P(2,2);
+
+end
+
+% Steady-State Kalman Gain
+K_SS = K;
+
+% Steady-State Covariance
+priorSS = priorP(end,:);
+postSS = postP(end,:);
+
+% Steady-State Estimator Poles
+% KFpoles = eig(K);
+
+% Position Estimate Plotting
+figure
+plot(t,x(1,:),'.')
+hold on
+plot(t,x_hat(1,:),'.')
+title('Position: Actual vs. Estimate')
+legend('Actual','Estimate')
+xlabel('Time (s)')
+ylabel('Value')
+
+% Covariance Plotting
+figure
+P1 = plot(t,priorP, 'b');
+hold on
+P2 = plot(t,postP, 'r');
+title('Covariance vs. Time')
+legend([P1(1) P2(1)],{'Prior','Posterior'})
+xlabel('Time (s)')
+ylabel('Covariance (units^2)')
+
+%% Part D
+
+
---
+++ startup.m
@@ -0,0 +1,18 @@
+%% startup.m
+
+% DESCRIPTION: startup.m is a file that is ran when MATLAB is started with
+% this working directory. Any desired commands can be ran during startup.
+% If switching to a new directory that contains a startup.m file in an open 
+% instance of MATLAB, type startup in the Command Window to call desired 
+% startup commands.
+
+%% Functionality
+
+% Add All Subfolders of Current Folder
+dir = fileparts(which(mfilename));
+addpath(genpath(dir))
+
+% Figure Settings
+set(0,'DefaultFigureWindowStyle', 'docked');
+set(groot, 'defaultLineLinewidth', 1.75);
+set(0, 'defaultAxesFontSize', 18);
